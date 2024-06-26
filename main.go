708eae9132682dcf6c5562fc3b5b73f6394512ae
Diego Gutierrez <diagutierrezro@unal.edu.co>
2024-06-26 16:36:54 -0500

    fix: Ajuste para conexión con db

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/url"
+
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	"github.com/astaxie/beego/plugins/cors"
@@ -17,7 +19,7 @@ func main() {
 	//orm.RegisterDataBase("default", "postgres", beego.AppConfig.String("sqlconn"))
 	orm.RegisterDataBase("default", "postgres", "postgres://"+
 		beego.AppConfig.String("PGuser")+":"+
-		beego.AppConfig.String("PGpass")+"@"+
+		url.QueryEscape(beego.AppConfig.String("PGpass"))+"@"+
 		beego.AppConfig.String("PGhost")+":"+
 		beego.AppConfig.String("PGport")+"/"+
 		beego.AppConfig.String("PGdb")+"?sslmode=disable&search_path="+
